9782c9ddf7675b696c64757d57aba6ff82664217
Mark Spannbauer <msmarkieer@gmail.com>
2020-04-16 11:31:06 -0500

    favorites now exists

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Favorites from '../Favorites/Favorites'
+
 
 
 class App extends Component {
@@ -7,6 +9,7 @@ class App extends Component {
     return (
       <div>
         <h1>Giphy Search!</h1>
+        <Favorites />
       </div>
     );
   }
---
+++ src/components/Favorites/Favorites.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+
+class Favorites extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <Card>
+                <CardContent>Hello</CardContent>
+                <CardActions>Hello</CardActions>
+            </Card>
+         );
+    }
+}
+ 
+export default Favorites;
